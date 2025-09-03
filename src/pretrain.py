fbf21d7cbd2d41cf1467f4a3886e1a2b8e3eeefa
George Vengrovski <georgevenven@gmail.com>
2025-09-02 17:14:00 -0700

    changed default train paramters

--- src/pretrain.py
+++ src/pretrain.py
@@ -493,11 +493,11 @@ if __name__ == "__main__":
     parser.add_argument("--run_name", type=str, help="directory name inside /runs to store train run details")
 
     # Defaults 
-    parser.add_argument("--steps", type=int, default=50_000, help="number of training steps")
+    parser.add_argument("--steps", type=int, default=500_000, help="number of training steps")
     parser.add_argument("--lr", type=float, default=2e-4, help="learning rate")
-    parser.add_argument("--batch_size", type=int, default=128, help="batch size")
+    parser.add_argument("--batch_size", type=int, default=1024, help="batch size")
     parser.add_argument("--patch_height", type=int, default=32, help="patch height")
-    parser.add_argument("--patch_width", type=int, default=4, help="patch width")
+    parser.add_argument("--patch_width", type=int, default=1, help="patch width")
     parser.add_argument("--mels", type=int, default=128, help="number of mel bins")
     parser.add_argument("--num_timebins", type=int, default=1024, help="n number of time bins")
     parser.add_argument("--dropout", type=float, default=0.1, help="dropout rate")
@@ -509,10 +509,10 @@ if __name__ == "__main__":
 
 
     # Encoder Model
-    parser.add_argument("--enc_hidden_d", type=int, default=192, help="encoder hidden dimension")
+    parser.add_argument("--enc_hidden_d", type=int, default=384, help="encoder hidden dimension")
     parser.add_argument("--enc_n_head", type=int, default=6, help="encoder number of attention heads")
     parser.add_argument("--enc_n_layer", type=int, default=6, help="encoder number of transformer layers")
-    parser.add_argument("--enc_dim_ff", type=int, default=768, help="encoder feed-forward dimension")
+    parser.add_argument("--enc_dim_ff", type=int, default=1536, help="encoder feed-forward dimension")
 
     # Decoder Model
     parser.add_argument("--dec_hidden_d", type=int, default=192, help="decoder hidden dimension")
